77eb20711338ad7aa5e863929b9d3e8a253be506
David González <dagonco.dev@gmail.com>
2022-07-08 10:29:51 +0200

    Commit

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ const storage = require('./azure-storage');
 const run = async () => {
     try {
       core.startGroup("getContainerClient()");
+      core.info("Workflow started!")
       const container = await storage.getContainerClient();
       core.info("Your container for this workflow is: " + container);
       core.setOutput("ci-container-name", container);
       core.endGroup();
     } catch (err) {
+      core.info("Workflow failed!");
       core.setFailed(err.message);
     }
   };
